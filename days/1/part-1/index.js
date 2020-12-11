0fc7a983e080b0829b61375e8ad7110a9d69a27e
Krzysztof Pawelec <ScriptyChris@gmail.com>
2020-12-11 12:13:47 +0100

    Calculate multiplication of two numbers, which sum is 2020;

--- days/1/part-1/index.js
+++ days/1/part-1/index.js
@@ -1,1 +1,25 @@
-console.log('script loaded!');
+const { resolve } = require('path');
+const { readFileSync } = require('fs');
+
+const input = readFileSync(resolve(__dirname,'./input.txt'), { encoding: 'utf8' });
+const numbersFromInput = input.match(/\d+/g);
+const numbersFromInputLen = numbersFromInput.length;
+
+const expected = 2020;
+let multiplicationResult = 0;
+
+outerLoop: for (let i = 0; i < numbersFromInputLen; i++) {
+  const left = numbersFromInput[i];
+
+  for (let j = 0; j < numbersFromInputLen; j++) {
+    const right = numbersFromInput[j];
+
+    if (+left + +right === expected) {
+      multiplicationResult = left * right;
+      break outerLoop;
+    }
+  }
+}
+
+console.log('numbersFromInput', numbersFromInput);
+console.log('multiplicationResult', multiplicationResult);
