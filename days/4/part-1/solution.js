f6e33486eb5970e61584c50e09f0dc7b59b03e23
Krzysztof Pawelec <ScriptyChris@gmail.com>
2020-12-14 00:26:56 +0100

    Implement solution for day 4 part 1;

--- days/4/part-1/solution.js
+++ days/4/part-1/solution.js
@@ -2,8 +2,27 @@ const { readFileSync } = require('fs');
 const { resolve } = require('path');
 const { EOL } = require('os');
 
+const numberOfAllPassportFields = 8;
+const optionalPassportField = 'cid';
 const passports = readFileSync(resolve(__dirname, '../input.txt'), { encoding: 'utf8' })
   .split(new RegExp(EOL.repeat(2), 'g'))
-  .map((data) => data.split(new RegExp(`\\s|${EOL}`, 'g')).filter(Boolean));
+  .map((data) => {
+    const dataArray = data.split(new RegExp(`\\s|${EOL}`, 'g')).filter(Boolean);
+    return dataArray.reduce((passport, dataEntry) => {
+      const [key, value] = dataEntry.split(':');
 
-console.log('passports', passports);
+      passport[key] = value;
+
+      return passport;
+    }, {});
+  });
+const passportsConsideredValid = passports.filter((passport) => {
+  const passportKeysLen = Object.keys(passport).length;
+
+  return (
+    passportKeysLen === numberOfAllPassportFields ||
+    (passportKeysLen === numberOfAllPassportFields - 1 && !(optionalPassportField in passport))
+  );
+});
+
+console.log('passports.length:', passports.length, ' /validPassports.length:', passportsConsideredValid.length);
